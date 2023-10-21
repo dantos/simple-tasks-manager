99ed9153bff612d15747afee598de629b42c03db
dantos <dantoscruz@gmail.com>
2023-10-21 14:21:43 -0400

    Adding basic draggable list

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import './jquery-3.6.0.js';
+import './jquery-ui.js';
+$( function() {
+  $( "#sortable" ).sortable({
+    update: function (e, ui) {
+      let index = ui.item.index();
+    }
+  });
+} );
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.basic')
+@section('content')
+  <ul id="sortable">
+    <li class="ui-state-default" data-id="1"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 1</li>
+    <li class="ui-state-default" data-id="2"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 2</li>
+    <li class="ui-state-default" data-id="3"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 3</li>
+    <li class="ui-state-default" data-id="4"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 4</li>
+    <li class="ui-state-default" data-id="5"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 5</li>
+    <li class="ui-state-default" data-id="6"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 6</li>
+    <li class="ui-state-default" data-id="7"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 7</li>
+  </ul>
+@endsection
